3d3492c6c873199b50b2c7d1e1504c33a020dc18
Tianon Gravi <admwiggin@gmail.com>
2025-02-14 14:23:11 -0800

    Slightly safer `.test/config.sh`

--- .test/config.sh
+++ .test/config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# https://github.com/docker-library/official-images/blob/618a85a87796c8bbea059011d94d888dc7554158/test/config.sh
+# https://github.com/docker-library/official-images/blob/3bc6a70175d4e1da2080b86415e6f3c8eb2c6af3/test/config.sh
 
-imageTests+=(
-	[bash]='bash-optional-features'
-)
+imageTests[bash]+='
+	bash-optional-features
+'
